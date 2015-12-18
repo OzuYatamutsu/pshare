87bc7ee615660b0026be8762623d4940c6c808d6
Sean Collins <seanc@seancotech.com>
2015-12-18 10:43:54 -0500

    Start Flask app

--- pshare.py
+++ pshare.py
@@ -1,4 +1,16 @@
 from socket import socket, AF_INET, SOCK_STREAM
+from flask import Flask, request
+
+# Output strings
+INIT_SHARE_MSG = "Your file is now accessible at thess URLs: "
+BASE_URL = "http://"
+
+app = Flask(__name__)
+
+app.route('/', "GET")
+def serve_file():
+    '''Serves the file to be shared.'''
+    pass
 
 def get_free_port_num():
     '''Gets a free port number to listen on.'''
@@ -10,4 +22,10 @@ def get_free_port_num():
     return port
 
 if __name__ == "__main__":
-    print(get_free_port_num())
+    # Start Flask app
+    port = get_free_port_num()
+    app.run(port=port)
+    
+    print(INIT_SHARE_MSG)
+    # TODO: Detect local and public IP address
+    print(BASE_URL + "localhost:" + port)
